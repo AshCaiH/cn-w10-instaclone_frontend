a6d652a82e0b00b9d40e218c7577aaccdfa9952c
Ash <56586401+AshCaiH@users.noreply.github.com>
2024-02-28 16:53:41 +0000

    feature: image grid layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,17 @@ import Navbar from './components/navbar/_main'
 import LoginRegister from './components/login-register/_main'
 import ImageGrid from './components/images/image-grid/_main'
 
-function App() {
 
-  return (
-    <>
-      < Navbar />
-      < LoginRegister />
-      < ImageGrid />
-    </>
-  )
+
+function App() {    
+
+    return (
+        <>
+            < Navbar />
+            < ImageGrid />
+            < LoginRegister />
+        </>
+    )
 }
 
 export default App
---
+++ src/components/images/image-grid/image-thumb.jsx
@@ -0,0 +1,26 @@
+// props
+// - image
+
+export const ImageThumb = (props) => {
+    const randomSide = () => {
+        if (Math.random() > 0.5) return "100%";
+        else return "0";
+    }
+
+    const randomAngle = (limit) => {
+        return (Math.random() * limit - (limit/2)) + "deg"
+    }
+
+    return (
+        <div className="polaroid" style={{
+            rotate: randomAngle(12),
+        }}>
+            <div className="image-thumb" style={{
+                backgroundImage: `url(${props.image.urlSmall})`,
+                backgroundPositionX: randomSide(),
+            }} />
+        </div>
+    )
+}
+
+export default ImageThumb
